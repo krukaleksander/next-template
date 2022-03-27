ab4819de165a891d102937db676ed26afd8af872
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-27 20:22:35 +0200

    edit index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,3 @@
-import Link from 'next/link'
-
 export default function Home() {
-  return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+  return <div>New next app</div>
 }
